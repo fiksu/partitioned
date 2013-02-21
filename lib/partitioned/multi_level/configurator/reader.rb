9bd5fb0a01ebae97633a700f1e2aab1c6995c52b
Edward Slavich <eslavich@fiksu.com>
2013-02-21 10:11:40 -0500

    Create new derived classes for multi-level so different partitioned models don't try to interact with each other.

--- lib/partitioned/multi_level/configurator/reader.rb
+++ lib/partitioned/multi_level/configurator/reader.rb
@@ -187,13 +187,18 @@ module Partitioned
 
         def set_up_classes_and_configurators
           if is_primary?
-            @using_classes = collect_from_collection(&:using_classes).inject([]) do |array,new_items|
+            abstract_classes = collect_from_collection(&:using_classes).inject([]) do |array,new_items|
               array += [*new_items]
-            end.to_a
+            end.to_a             
             
-            @using_configurators = @using_classes.map do |partition_class|
+            @using_classes = abstract_classes.map do |abstract_class| 
+              partition_class = Class.new(abstract_class)
+              partition.table_name = self.model.table_name
               partition_class.configurator = self.class.new(partition_class, self)
-              partition_class.table_name = self.model.table_name
+              partition_class
+            end
+
+            @using_configurators = @using_classes.map do |partition_class|
               partition_class::Configurator::Reader.new(partition_class)
             end
 
